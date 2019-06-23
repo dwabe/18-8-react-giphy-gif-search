1cf9edf91e8bada194c6a22dab438af452511c96
Bartek Błęcki <bartek@blecki.pl>
2019-06-23 18:22:09 +0200

    Update Search.js

--- components/Search.js
+++ components/Search.js
@@ -10,8 +10,17 @@ Search = React.createClass({
         this.setState({
             searchingText: searchingText
         });
+        if (searchingText.length > 2) {
+            this.props.onSearch(searchingText);
+        }
     },
-    
+
+    handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+            this.props.onSearch(this.state.searchingText);
+        }
+    },
+
     render: function() {
         var styles = {
             fontSize: '1.5em',
@@ -19,12 +28,21 @@ Search = React.createClass({
             maxWidth: '350px'
         };
 
-    return <input
-             type="text"
-             onChange={this.handleChange}
-             placeholder="Tutaj wpisz wyszukiwaną frazę"
-             style={styles}
-             value={this.state.searchTerm}
-            />
-  }
+        return <input
+        type = 'text'
+        onChange = {
+            this.handleChange
+        }
+        onKeyUp = {
+            this.handleKeyUp
+        }
+        placeholder = 'Tutaj wpisz wyszukiwaną frazę'
+        style = {
+            styles
+        }
+        value = {
+            this.state.searchTerm
+        }
+        />
+    }
 });
